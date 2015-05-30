211f3110f40014321c8fd50408e6523708c1ef62
takahirom <takam.dev@gmail.com>
2015-05-30 22:30:08 +0900

    onclicklistener to data binding

--- app/src/main/java/advancedandroid/databindingsamples/MainActivity.java
+++ app/src/main/java/advancedandroid/databindingsamples/MainActivity.java
@@ -1,50 +1,36 @@
 package advancedandroid.databindingsamples;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import advancedandroid.databindingsamples.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.variable_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, VariableBindingActivity.class));
-            }
-        });
-        findViewById(R.id.custom_binding_class_name_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            }
-        });
+        final ActivityMainBinding viewDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        viewDataBinding.setClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View view) {
+        final int id = view.getId();
+        if (id == R.id.variable_button) {
+            startActivity(new Intent(MainActivity.this, VariableBindingActivity.class));
+            return;
         }
-
-        return super.onOptionsItemSelected(item);
+        if (id == R.id.custom_binding_observable) {
+            Toast.makeText(this, "not implemented", Toast.LENGTH_SHORT).show();
+        }
     }
+
 }
